afd0291dd102a468cd100e97780e7f9d7732bcd2
PECman <762420244@qq.com>
2018-03-24 17:58:25 +0800

    Add files via upload
    主要代码

---
+++ GreatCaptain.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace GreatCaptain
+{
+    partial class GreatCaptain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GreatCaptain));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.processes = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pids = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(82, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "进程关闭器";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "进程名：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Kill";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // processes
+            // 
+            this.processes.FormattingEnabled = true;
+            this.processes.ItemHeight = 12;
+            this.processes.Location = new System.Drawing.Point(305, 68);
+            this.processes.Name = "processes";
+            this.processes.Size = new System.Drawing.Size(250, 184);
+            this.processes.TabIndex = 4;
+            this.processes.SelectedIndexChanged += new System.EventHandler(this.processes_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Refresh";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(302, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "进程名";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(581, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "PID";
+            // 
+            // pids
+            // 
+            this.pids.FormattingEnabled = true;
+            this.pids.ItemHeight = 12;
+            this.pids.Location = new System.Drawing.Point(584, 68);
+            this.pids.Name = "pids";
+            this.pids.Size = new System.Drawing.Size(250, 184);
+            this.pids.TabIndex = 8;
+            this.pids.SelectedIndexChanged += new System.EventHandler(this.pids_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe Print", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "@PECman QQ:762420244";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("仿宋", 10F);
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(529, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(371, 14);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "未经许可，除作者以外的任何人禁止用程序以任何方式获利";
+            // 
+            // GreatCaptain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 291);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.pids);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.processes);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "GreatCaptain";
+            this.Text = "GreatCaptain";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox processes;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox pids;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ GreatCaptain.cs
@@ -0,0 +1,76 @@
+﻿/*
+ * Author: PECman
+ * Date: 2018/3/24
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace GreatCaptain
+{
+    public partial class GreatCaptain : Form
+    {
+        public GreatCaptain()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (Process.GetProcessesByName(textBox1.Text).Length > 0)
+                {
+                    foreach (Process pro in Process.GetProcessesByName(textBox1.Text))
+                    {
+                        pro.Kill();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("该进程不存在！");
+                }
+            }
+            else
+            {
+                MessageBox.Show("进程名有误！");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (Process pro in Process.GetProcesses())
+            {
+                processes.Items.Add(pro.ProcessName);
+                pids.Items.Add(pro.Id);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            processes.Items.Clear();
+            pids.Items.Clear();
+            foreach (Process pro in Process.GetProcesses())
+            {
+                processes.Items.Add(pro.ProcessName);
+                pids.Items.Add(pro.Id);
+            }
+        }
+
+        private void processes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pids.SelectedIndex = processes.SelectedIndex;
+        }
+
+        private void pids_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            processes.SelectedIndex = pids.SelectedIndex;
+        }
+    }
+}
